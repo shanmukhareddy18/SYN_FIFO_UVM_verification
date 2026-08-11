4c8938e20e76a81d80874f7080ff95477b35c39c
shanmukhareddy18 <shanmukhareddyshan@gmail.com>
2026-08-11 09:57:38 +0530

    pp

---
+++ src/testbench/fifo_pkg.sv
@@ -0,0 +1,19 @@
+package fifo_pkg;
+ import uvm_pkg::*;
+ `include "uvm_macros.svh"
+ 
+ `include "defines.svh"
+ `include "seq_i.sv"
+ `include "sequences.sv"
+ `include "sequencer.sv"
+ `include "driver.sv"
+ `include "input_monitor.sv"
+ `include "output_monitor.sv"
+ `include "agent_active.sv"
+ `include "agent_passive.sv"
+ `include "scoreboard.sv"
+ `include "subscriber.sv"
+ `include "env.sv"
+ `include "test.sv"
+ 
+endpackage
